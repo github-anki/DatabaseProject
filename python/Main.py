5e35afb1d3456b50d030540d226d7ba2f046295d
Burdzi0 <burdzi0@programmer.net>
2019-11-04 22:37:16 +0100

    Add university and user

--- python/Main.py
+++ python/Main.py
@@ -8,7 +8,27 @@ engine = create_engine('postgres://docker:docker@localhost/docker', echo=True)
 
 meta.create_all(engine)
 
-students = Table(
+# CREATE TABLE "university" (
+#     "university_id" serial NOT NULL,
+#     "uni_name" VARCHAR(50) NOT NULL,
+#     "postal_code" VARCHAR(10) NOT NULL,
+#     "town" VARCHAR(100) NOT NULL,
+#     "address" VARCHAR(100) NOT NULL,
+#     "country" VARCHAR(50) NOT NULL,
+#     CONSTRAINT "university_pk" PRIMARY KEY ("university_id")
+# );
+
+university = Table(
+    'university', meta,
+    Column('university_id', Integer, primary_key=True),
+    Column('uni_name', String),
+    Column('postal_code', String),
+    Column('town', String),
+    Column('address', String),
+    Column('country', String)
+)
+
+user = Table(
     'user', meta,
     Column('user_id', Integer, primary_key=True),
     Column('university_id', Integer),
@@ -19,5 +39,22 @@ students = Table(
 )
 
 conn = engine.connect()
-ins = students.insert().values(university_id=1, name='Ravi', surname='Surname', email='a@a.com', registration_date=date.today())
-result = conn.execute(ins)
+
+# Create university
+insert_university = university.insert().values(uni_name='PWr', postal_code='50-353', town='Wrocław', address='Address', country='Poland')
+
+# Insert university
+conn.execute(insert_university)
+
+# Select query
+query = 'SELECT university_id from university'
+
+# Select all universities
+university_ids = conn.execute(query).fetchall()
+
+for row in university_ids:
+    print(row[0])
+
+insert_user = user.insert().values(university_id=university_ids[0][0], name='Ravi', surname='Surname', email='a@a.com', registration_date=date.today())
+
+conn.execute(insert_user)
