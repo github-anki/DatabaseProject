0630231e0e2e5ecdce81b5332383717c6d58fd43
Paul <45698887+PaulTony7@users.noreply.github.com>
2019-11-04 23:04:36 +0100

    Pul add lecture timetable classroom paper event

--- python/Main.py
+++ python/Main.py
@@ -29,6 +29,54 @@ user = Table(
     Column('registration_date', Date)
 )
 
+
+event = Table(
+    'event', meta,
+    Column('event_id', Integer, primary_key=True),
+    Column('event_name', String),
+    Column('organizing_body', String),
+    Column('address', String),
+    Column('postal_code', String),
+    Column('town', String)
+)
+
+timetable = Table(
+    'timetable', meta,
+    Column('timetable_id', Integer, primary_key=True),
+    Column('event_id', Integer),
+    Column('timetable_name', String),
+    Column('date', Date)
+)
+
+classroom = Table(
+    'classroom', meta,
+    Column('classroom_id', Integer, primary_key=True),
+    Column('classroom_number', Integer),
+    Column('postal_code', String),
+    Column('town', String),
+    Column('address', String)
+)
+
+lecture = Table(
+    'lecture', meta,
+    Column('lecture_id', Integer, primary_key=True),
+    Column('user_id', Integer),
+    Column('classroom_id', Integer),
+    Column('timetable_id', Integer),
+    Column('paper_id', Integer),
+    Column('start_time', Time),
+    Column('duration', Integer)
+)
+
+paper = Table(
+    'paper', meta,
+    Column('paper_id', Integer, primary_key=True),
+    Column('user_id', Integer),
+    Column('domain_id', Integer),
+    Column('topic', String),
+    Column('abstrakt', String)
+)
+
 conn = engine.connect()
 fake = Faker()
 
